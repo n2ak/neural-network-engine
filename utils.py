4bc3c372fc799f7a8fb6e4fd1cb7ce1f7b3927ad
n2ak <n2akajdaou@gmail.com>
2025-10-01 12:24:55 +0000

    transform dataloader to be a class

--- utils.py
+++ utils.py
@@ -1,14 +1,31 @@
-
 import numpy as np
 
 
-def dataloader(*arrs: np.typing.NDArray, batch_size=64, shuffle=True):
-    idx = 0
-    dlen = arrs[0].shape[0]
-    if shuffle:
-        perm = np.random.permutation(dlen)
-        arrs = tuple(arr[perm] for arr in arrs)
+class DataLoader:
+    def __init__(self, *arrs: np.typing.NDArray, batch_size=64, shuffle=True):
+        self.arrs = arrs
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.dlen = arrs[0].shape[0]
+        self.idx = 0
+        self._prepare()
 
-    while idx < dlen:
-        yield tuple(arr[idx:idx+batch_size] for arr in arrs)
-        idx += batch_size
+    def _prepare(self):
+        if self.shuffle:
+            perm = np.random.permutation(self.dlen)
+            self.arrs = tuple(arr[perm] for arr in self.arrs)
+        self.idx = 0
+
+    def __len__(self): return self.dlen // self.batch_size
+
+    def __iter__(self):
+        self._prepare()
+        return self
+
+    def __next__(self):
+        if self.idx >= self.dlen:
+            raise StopIteration
+        batch = tuple(arr[self.idx:self.idx+self.batch_size]
+                      for arr in self.arrs)
+        self.idx += self.batch_size
+        return batch
